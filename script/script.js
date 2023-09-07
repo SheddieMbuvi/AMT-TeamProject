4c96b3f097aebc09931912980b76371ce64e7fee
NyangoyaG <nyangoyagb@gmail.com>
2023-09-07 13:17:39 -0700

    new files

--- script/script.js
+++ script/script.js
@@ -1,49 +1,11 @@
-const loginForm = document.getElementById("loginForm");
-const signupForm = document.getElementById("signupForm");
-const loginToggle = document.getElementById("loginToggle");
-const signupToggle = document.getElementById("signupToggle");
-const forgotPasswordLink = document.getElementById("forgotPasswordLink");
-const forgotPasswordModal = document.getElementById("forgotPasswordModal");
-const closeModal = document.getElementById("closeModal");
+const signUpButton = document.getElementById('signUp')
+const signInButton = document.getElementById('signIn')
+const container = document.getElementById('container')
 
-loginToggle.addEventListener("click", () => {
-    loginForm.classList.add("active");
-    signupForm.classList.remove("active");
-});
-
-signupToggle.addEventListener("click", () => {
-    signupForm.classList.add("active");
-    loginForm.classList.remove("active");
-});
-
-forgotPasswordLink.addEventListener("click", () => {
-    forgotPasswordModal.style.display = "block";
-});
-
-closeModal.addEventListener("click", () => {
-    forgotPasswordModal.style.display = "none";
-});
-
-window.addEventListener("click", (event) => {
-    if (event.target === forgotPasswordModal) {
-        forgotPasswordModal.style.display = "none";
-    }
-});
-
-// Handle form submissions (placeholders, no actual functionality)
-loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    alert("Login form submitted");
-});
-
-signupForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    alert("Signup form submitted");
-});
-
-forgotPasswordModal.addEventListener("submit", (event) => {
-    event.preventDefault();
-    alert("Password reset request submitted");
-    forgotPasswordModal.style.display = "none";
-});
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active")
+	})
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active")
+	})
 
